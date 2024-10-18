5c9677d684fb40f18d226ad12b1347084bc5c51b
Mark Goltsman <goltsmannn@lawn-143-215-110-145.lawn.gatech.edu>
2024-10-18 01:09:09 -0400

    Implemented create and copy functions using premise syntax and synchronous syntaz

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const pathFrom = 'files';
+    const pathTo = 'files_copy';
+    fs.access(pathFrom)
+        .then((data)=>{
+            fs.access(pathTo)
+                .then((exists)=>{
+                    console.error('FS operation failed')
+                })
+                .catch((err)=>{
+                    fs.mkdir(pathTo, { recursive: true })
+                        .then(
+                            (path) => {
+                                fs.cp(pathFrom, pathTo, { recursive: true });
+                            }
+                        )
+
+                })
+        })
+        .catch((error)=>{
+            console.error('FS operation failed')
+        });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
 const create = async () => {
-    // Write your code here 
+    const path = 'files/fresh.txt';
+    const data = "I am fresh and young";
+    try {
+        fs.access(path, (err) => {
+            if (err) {
+                fs.writeFile(path, data, (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+            } else {
+                throw new Error('FS operation failed');
+            }
+        });
+    }
+    catch (err) {
+        console.error(err);
+    }
 };
 
 await create();
