0d97b017a1aa0b66d387a1ba6fdf631e9b84ad93
Mark Goltsman <mmgoltsman@mail.ru>
2024-10-21 16:23:49 -0400

    Hash Module Done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import path from "path";
+import fs from "fs";
+import {fileURLToPath} from "url";
+import * as crypto from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+    const readStream = fs.createReadStream(filePath)
+        .on('data', (chunk) => {data += chunk.toString().trim()})
+        .on('end', ()=>{
+            const response = crypto.createHash('SHA-256').update(data).digest('hex');
+            process.stdout.write(response.toString());
+        });
+    let data = "";
+
 };
 
 await calculateHash();
