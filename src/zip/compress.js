ab1c013615d9b87f631be87305d55dc714503a3a
Mark Goltsman <mmgoltsman@mail.ru>
2024-10-21 19:11:36 -0400

    Completed childprocesses, threads, and hash. Finished the assignment

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import stream from "node:stream";
+import fs from "fs";
+import zlib from "zlib";
+import {fileURLToPath} from "url";
+import path from "path";
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files/fileToCompress.txt');
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream('files/archive.gz').on('data', (chunk) => {process.stdout.write(chunk.toString())});
+
+    const transformStream = new stream.Transform({
+        transform(data, encoding, callback) {
+            zlib.brotliCompress(data.toString().trim(), (err, compressedData) =>{
+                this.push(compressedData);
+                callback();
+            });
+        }
+    })
+    readStream.pipe(transformStream).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import stream from "node:stream";
+import fs from "fs";
+import zlib from "zlib";
+import {fileURLToPath} from "url";
+import path from "path";
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files/archive.gz');
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream('files/decompressedFile.txt');
+
+    const transformStream = new stream.Transform({
+        transform(data, encoding, callback) {
+            zlib.brotliDecompress(data, (err, decompressedData) =>{
+                this.push(decompressedData);
+                callback();
+            })
+        }})
+    readStream.pipe(transformStream).pipe(writeStream);
 };
 
 await decompress();
