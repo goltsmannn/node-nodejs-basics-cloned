c3e202e814588a955f00aa3ae26bba0350665926
Mark Goltsman <mmgoltsman@mail.ru>
2024-10-21 01:55:30 -0400

    Completed CLI and modules tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import {argv} from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+    const pairs = argv.map((value, index) =>
+        {
+            if(index % 2 === 0 && index < argv.length - 1){
+                return `${value.slice(2)} is ${argv[index+1]}'`;
+            }
+        }
+    ).join(', ');
+    console.log(pairs);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+import {env} from "node:process";
 const parseEnv = () => {
-    // Write your code here 
+    // for (const varName in process.env) {
+    //     if(varName.startsWith('RSS_')){
+    //         process.stdout.write(`${varName} ${process.env[varName]};`); //костыльно уже просто жесть
+    //     }
+    // }
+    const ans = Object.keys(process.env)
+        .filter(key => key.startsWith("RSS_"))
+        .map(key => `${key} ${process.env[key]};`)
+        .join(' ')
+    console.log(ans);
 };
 
 parseEnv();
